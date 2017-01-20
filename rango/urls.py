97d7c8ba06e0732a517366df13924a743a434d55
David Law <2287182l@student.gla.ac.uk>
2017-01-20 18:31:11 +0000

    Practice Run

--- rango/urls.py
+++ rango/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+from rango import views
 
 app_rango = 'Rango'
 
