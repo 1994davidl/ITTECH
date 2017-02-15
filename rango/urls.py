663de91e4d031635617c1d4d3fb41ceca0534c48
David Law <2287182l@student.gla.ac.uk>
2017-02-15 15:48:14 +0000

    ammendments chapter 8

--- rango/urls.py
+++ rango/urls.py
@@ -5,6 +5,7 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
     url(r'^about/', views.about, name = 'about'),
 
     url(r'^add_category/$', views.add_category, name='add_category'),
